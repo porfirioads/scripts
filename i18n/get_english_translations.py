b81d47ca1358050a58a6efc547c5c88d417a24a9
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2023-03-13 14:35:28 -0600

    feat: setup project as a python project

--- i18n/get_english_translations.py
+++ i18n/get_english_translations.py
@@ -1,3 +1,4 @@
+import os.path
 from googletrans import Translator
 import json
 import sys
@@ -8,7 +9,13 @@ if len(sys.argv) != 2:
 
 # Rutas de los archivos
 json_file_es = sys.argv[1]
-json_file_en = 'i18n/english.json'
+json_file_en = os.path.splitext(json_file_es)[0] + '_en.json'
+
+if os.path.isfile(json_file_en):
+    with open(json_file_en, 'r', encoding='utf-8') as f:
+        en_data = json.load(f)
+else:
+    en_data = {}
 
 # Cargar archivo en español
 with open(json_file_es, 'r', encoding='utf-8') as f:
@@ -16,26 +23,23 @@ with open(json_file_es, 'r', encoding='utf-8') as f:
 
 # Inicializar el traductor
 translator = Translator()
+translator.raise_Exception = True
 
 # Traducir las claves faltantes al inglés
 for key in es_data.keys():
     try:
         # Comprobar si la clave ya existe en el archivo en inglés
-        with open(json_file_en, 'r', encoding='utf-8') as f:
-            en_data = json.load(f)
         if key in en_data:
             continue
 
         # Traducir el valor de la clave al inglés
-        translation = translator.translate(es_data[key], dest='en').text
+        translation = translator.translate(
+            es_data[key], dest='en', src='es').text
 
         # Guardar la traducción en el archivo en inglés
-        with open(json_file_en, 'r+', encoding='utf-8') as f:
-            en_data = json.load(f)
-            en_data[key] = translation
-            f.seek(0)
+        en_data[key] = translation
+        with open(json_file_en, 'w', encoding='utf-8') as f:
             json.dump(en_data, f, ensure_ascii=False, indent=4)
-            f.truncate()
 
     except Exception as e:
         print(f'Error al traducir clave {key}: {e}')
