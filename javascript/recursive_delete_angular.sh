62eaf7569bbd4629b06ffd80c97af928e2955fee
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2023-08-21 11:51:31 -0600

    fix: set correct name of .angular folder

--- javascript/recursive_delete_angular.sh
+++ javascript/recursive_delete_angular.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Función recursiva para eliminar angular
+# Función recursiva para eliminar .angular
 function eliminar_angular {
     local dir="$1"
 
     # Verificar si el directorio existe
     if [ -d "$dir" ]; then
-        echo "Eliminando angular en: $dir"
-        find "$dir" -type d -name "angular" -exec sudo rm -rf {} +
+        echo "Eliminando .angular en: $dir"
+        find "$dir" -type d -name ".angular" -exec sudo rm -rf {} +
     else
         echo "Directorio no encontrado: $dir"
     fi
